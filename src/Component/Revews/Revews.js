e0a510570b9e13fb72fb7032ae8a4d37ebb13e90
suzitsarkar27 <96899998+suzitsarkar27@users.noreply.github.com>
2022-04-05 18:36:18 +0600

    complet-R

--- src/Component/Revews/Revews.js
+++ src/Component/Revews/Revews.js
@@ -1,4 +1,5 @@
 import React from "react";
+import "./Reveiw.css";
 import useCostomarReviw from "../Hooks/useCostomarReviw";
 import { FaStar } from "react-icons/fa";
 
@@ -6,14 +7,22 @@ const Revews = () => {
   const [data, setData] = useCostomarReviw([]);
 
   return (
-    <div className="row">
+    <div className="row m-5">
       {data.map((data) => (
-        <div className="col-md-4">name:{data.name}</div>
+        <div className="col-md-4 reveiw-infro card g-5">
+          <img className="w-50" src={data.img} alt="" />
+          <h3>{data.name}</h3>
+          <p>
+            <FaStar className="icon-style"></FaStar>
+            <FaStar className="icon-style"></FaStar>
+            <FaStar className="icon-style"></FaStar>
+            <FaStar className="icon-style"></FaStar>
+            <FaStar className="icon-style"></FaStar>
+          </p>
+          <h3>{data.Ranting}</h3>
+          <p>{data.descripton}</p>
+        </div>
       ))}
-      <h2>this is revews items</h2>
-      <i class="fa-solid fa-star"></i>
-      <i class="fa-solid fa-star-sharp"></i>
-      <FaStar></FaStar>
     </div>
   );
 };
